eead56cdd346fed5e5b3c58275708f55cfa678c8
sygegsznnsns <3191958649@qq.com>
2025-11-20 23:23:27 +0800

    初始测试

--- src/wp-content/plugins/musicalbum-integrations/musicalbum-integrations.php
+++ src/wp-content/plugins/musicalbum-integrations/musicalbum-integrations.php
@@ -10,22 +10,24 @@ defined('ABSPATH') || exit;
 
 final class Musicalbum_Integrations {
     public static function init() {
-        add_action('init', [__CLASS__, 'register_shortcodes']);
-        add_action('wp_enqueue_scripts', [__CLASS__, 'enqueue_assets']);
+        add_action('init', array(__CLASS__, 'register_shortcodes'));
+        add_action('wp_enqueue_scripts', array(__CLASS__, 'enqueue_assets'));
         // 示例：与第三方插件交互（替换为实际钩子）
         // add_filter('some_plugin_output', [__CLASS__, 'filter_some_plugin_output'], 10, 1);
     }
 
     public static function register_shortcodes() {
-        add_shortcode('musicalbum_hello', function($atts = [], $content = '') {
-            return '<div class="musicalbum-hello">Hello Musicalbum</div>';
-        });
+        add_shortcode('musicalbum_hello', array(__CLASS__, 'shortcode_musicalbum_hello'));
+    }
+
+    public static function shortcode_musicalbum_hello($atts = array(), $content = '') {
+        return '<div class="musicalbum-hello">Hello Musicalbum</div>';
     }
 
     public static function enqueue_assets() {
-        wp_register_style('musicalbum-integrations', plugins_url('assets/integrations.css', __FILE__), [], '0.1.0');
+        wp_register_style('musicalbum-integrations', plugins_url('assets/integrations.css', __FILE__), array(), '0.1.0');
         wp_enqueue_style('musicalbum-integrations');
-        wp_register_script('musicalbum-integrations', plugins_url('assets/integrations.js', __FILE__), ['jquery'], '0.1.0', true);
+        wp_register_script('musicalbum-integrations', plugins_url('assets/integrations.js', __FILE__), array('jquery'), '0.1.0', true);
         wp_enqueue_script('musicalbum-integrations');
     }
 
